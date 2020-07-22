7872d2e104603918505002e19b0bd408591fbff9
Naoto YAMAGUCHI <i33399_YAMAGUCHI@aisin-aw.co.jp>
2020-07-22 20:01:57 +0900

    Update

--- rpmtest.py
+++ rpmtest.py
@@ -4,55 +4,49 @@ import sys
 import os
 from pyrpm.spec import Spec, replace_macros
 
-args = sys.argv
 
-if 1 >= len(args):
-	print('no args.')
-	exit()
+def create_git_am_sh(spec, specfn):
+	gitshfn = specfn.replace('.spec', '.sh')
 
-if os.path.isfile(args[1]) == False:
-	print('not find input file.')
-	exit()
-
-specfn = args[1];
-summaryfn = specfn.replace('.spec', '.log');
-gitshfn = specfn.replace('.spec', '.sh');
-bbincludefn = specfn.replace('.spec', '-centos.inc');
-
-spec = Spec.from_file(specfn)
-
-gitshfile = open(gitshfn,'w')
-if gitshfile:
-	for patch in spec.patches:
-		tmp = 'git am patch/' + patch + '\n'
-		gitshfile.writelines(tmp)
-	gitshfile.close()
+	gitshfile = open(gitshfn,'w')
+	if gitshfile:
+		for patch in spec.patches:
+			tmp = 'git am patch/' + patch + '\n'
+			gitshfile.writelines(tmp)
+		gitshfile.close()
+	return
 
 
-bbincludefile = open(bbincludefn,'w')
-if bbincludefile:
-	bbincludefile.writelines('CENTOS_PATCHES = \" \\\n')
+def create_bitbake_include(spec, specfn):
+	bbincludefn = specfn.replace('.spec', '-centos.inc')
+
+	bbincludefile = open(bbincludefn,'w')
+	if bbincludefile:
+		bbincludefile.writelines('CENTOS_PATCHES = \" \\\n')
+		
+		for patch in spec.patches:
+			tmp = '\t file://' + patch + ' \\\n'
+			bbincludefile.writelines(tmp)
 	
-	for patch in spec.patches:
-		tmp = '\t file://' + patch + ' \\\n'
-		bbincludefile.writelines(tmp)
+		bbincludefile.writelines('\t\"\n')
+		
+		bbincludefile.close()
 
-	bbincludefile.writelines('\t\"\n')
+
+def main():
+	args = sys.argv
 	
-	bbincludefile.close()
+	if 1 >= len(args):
+		print('no args.')
+		exit()
+	
+	if os.path.isfile(args[1]) == False:
+		print('not find input file.')
+		exit()
 
-#print(spec.version)
+	specfn = args[1];
+	spec = Spec.from_file(specfn)
 
-#for source in spec.sources:
-#	print(source)
-#	print(replace_macros(source, spec))
+	create_git_am_sh(spec, specfn)
+	create_bitbake_include(spec, specfn)
 
-
-#for patch in spec.patches:
-#	print("git am patch/",patch,sep='')
-
-
-#print(spec.changelog)
-
-#for package in spec.packages:
-#	print(f'{package.name}: {package.summary if hasattr(package, "summary") else spec.summary}')
